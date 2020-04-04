3ddc5e41cc520abf0a24690e9e7148bc2c3bc573
TimaLebed <timalebedevskiy@mail.ru>
2020-04-04 17:51:09 +0300

    feat: add data object

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const markup = `
             <span class="key">p</span>
             <span class="key">[</span>
             <span class="key">]</span>
-            <span class="key">\</span>
+            <span class="key">\\</span>
             <span class="key">del</span>
         </div>
         <div class="keyboard-row">
@@ -49,7 +49,7 @@ const markup = `
             <span class="key">k</span>
             <span class="key">l</span>
             <span class="key">;</span>
-            <span class="key">'</span>
+            <span class="key">&#8242</span>
             <span class="key which-13">enter</span>
         </div>
         <div class="keyboard-row">
@@ -104,5 +104,284 @@ keyBoard.addEventListener('mouseup', event => {
 
 
 
+const state = {
+    // ru: true,
+    en: false
+};
 
-console.log(keyArray); 
+
+// const markup = `
+//     <div class="container">
+//         <span class="con">${data[81].keyName[state.ru ? 'ru' : 'en']}</span>
+//     </div>
+// `;
+
+
+const data = {
+    81: {
+        keyName: {
+            ru: 'й',
+            en: 'q'
+        },
+        isLetter: true
+    },
+    87: {
+        keyName: {
+            ru: 'ц',
+            en: 'w'
+        },
+        isLetter: true
+    },
+    69: {
+        keyName: {
+            ru: 'у',
+            en: 'e'
+        },
+        isLetter: true
+    },
+    82: {
+        keyName: {
+            ru: 'к',
+            en: 'r'
+        },
+        isLetter: true
+    },
+    84: {
+        keyName: {
+            ru: 'е',
+            en: 't'
+        },
+        isLetter: true
+    },
+    89: {
+        keyName: {
+            ru: 'н',
+            en: 'y'
+        },
+        isLetter: true
+    },
+    85: {
+        keyName: {
+            ru: 'г',
+            en: 'u'
+        },
+        isLetter: true
+    },
+    73: {
+        keyName: {
+            ru: 'ш',
+            en: 'i'
+        },
+        isLetter: true
+    },
+    79: {
+        keyName: {
+            ru: 'Щ',
+            en: 'o'
+        },
+        isLetter: true
+    },
+    80: {
+        keyName: {
+            ru: 'з',
+            en: 'p'
+        },
+        isLetter: true
+    },
+    219: {
+        keyName: {
+            ru: 'х',
+            en: '['
+        },
+        isLetter: true
+    },
+    221: {
+        keyName: {
+            ru: 'ъ',
+            en: ']'
+        },
+        isLetter: true
+    },
+    220: {
+        keyName: {
+            ru: '\\',
+            en: '/'
+        },
+        isLetter: true
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    65: {
+        keyName: {
+            ru: 'ф',
+            en: 'a'
+        },
+        isLetter: true
+    },
+    83: {
+        keyName: {
+            ru: 'ы',
+            en: 's'
+        },
+        isLetter: true
+    },
+    68: {
+        keyName: {
+            ru: 'в',
+            en: 'd'
+        },
+        isLetter: true
+    },
+    70: {
+        keyName: {
+            ru: 'а',
+            en: 'f'
+        },
+        isLetter: true
+    },
+    71: {
+        keyName: {
+            ru: 'п',
+            en: 'g'
+        },
+        isLetter: true
+    },
+    72: {
+        keyName: {
+            ru: 'р',
+            en: 'h'
+        },
+        isLetter: true
+    },
+    74: {
+        keyName: {
+            ru: 'о',
+            en: 'j'
+        },
+        isLetter: true
+    },
+    75: {
+        keyName: {
+            ru: 'л',
+            en: 'k'
+        },
+        isLetter: true
+    },
+    76: {
+        keyName: {
+            ru: 'д',
+            en: 'l'
+        },
+        isLetter: true
+    },
+    186: {
+        keyName: {
+            ru: 'ж',
+            en: ';'
+        },
+        isLetter: true
+    },
+    222: {
+        keyName: {
+            ru: 'э',
+            en: '&#8242'
+        },
+        isLetter: true
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    90: {
+        keyName: {
+            ru: 'я',
+            en: 'z'
+        },
+        isLetter: true
+    },
+    88: {
+        keyName: {
+            ru: 'ч',
+            en: 'x'
+        },
+        isLetter: true
+    },
+    67: {
+        keyName: {
+            ru: 'с',
+            en: 'c'
+        },
+        isLetter: true
+    },
+    86: {
+        keyName: {
+            ru: 'м',
+            en: 'v'
+        },
+        isLetter: true
+    },
+    66: {
+        keyName: {
+            ru: 'и',
+            en: 'b'
+        },
+        isLetter: true
+    },
+    78: {
+        keyName: {
+            ru: 'т',
+            en: 'n'
+        },
+        isLetter: true
+    },
+    77: {
+        keyName: {
+            ru: 'ь',
+            en: 'm'
+        },
+        isLetter: true
+    },
+    188: {
+        keyName: {
+            ru: 'б',
+            en: ','
+        },
+        isLetter: true
+    },
+    222: {
+        keyName: {
+            ru: 'ю',
+            en: '.'
+        },
+        isLetter: true
+    },
+    222: {
+        keyName: {
+            ru: '.',
+            en: '/'
+        },
+        isLetter: true
+    },
+};
